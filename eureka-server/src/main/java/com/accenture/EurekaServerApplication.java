aa9e0e030c5054997a0f3b08f711d19fb2f5f08c
shuxing <shuxing.liu@foxmail.com>
2017-04-01 14:26:17 +0800

    Eureka Server

---
+++ eureka-server/src/main/java/com/accenture/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.accenture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ *
+ * @author darren.shuxing.liu
+ * @email  darren.shuxing.liu@accenture.com shuxing.liu@gmail.com
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}

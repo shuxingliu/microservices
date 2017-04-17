d9553e83317a17e31615a4f38184a2c0f9ffb82a
shuxing <shuxing.liu@foxmail.com>
2017-04-17 09:24:01 +0800

    service gateway

---
+++ zuul-service/src/main/java/com/accenture/j2c/gateway/ZuulServiceApplication.java
@@ -0,0 +1,12 @@
+package com.accenture.j2c.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulServiceApplication.class, args);
+	}
+}

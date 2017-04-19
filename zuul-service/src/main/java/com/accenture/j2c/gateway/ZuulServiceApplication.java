6471f0e0eb4040745d2f4edab31f26f3d2c69f77
shuxing <shuxing.liu@foxmail.com>
2017-04-19 23:02:28 +0800

    refine gateway service

--- zuul-service/src/main/java/com/accenture/j2c/gateway/ZuulServiceApplication.java
+++ zuul-service/src/main/java/com/accenture/j2c/gateway/ZuulServiceApplication.java
@@ -2,8 +2,10 @@ package com.accenture.j2c.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class ZuulServiceApplication {
 
 	public static void main(String[] args) {
